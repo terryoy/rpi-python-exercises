b8b75010ed98ea14ad3cd2b65ab770a640d79121
terryoy <terry.ouyang@gmail.com>
2015-09-06 19:51:01 +0800

    add more comments to gpio.py

--- gpio/gpio.py
+++ gpio/gpio.py
@@ -1,13 +1,15 @@
+# This program turns on the LED on pin 4 for 3 seconds and then turn off
+
 import RPi.GPIO as GPIO
 import time
 
-ledPin = 4
+ledPin = 4 # define the LED pin
 
 GPIO.setmode(GPIO.BCM) # set Broadcom's pin numbering scheme
 GPIO.setup(ledPin, GPIO.OUT) # set led pin to OUTPUT mode
 GPIO.output(ledPin, GPIO.HIGH) # pull up the pin
 
-time.sleep(3)
+time.sleep(3) # wait for 3 seconds
 
 GPIO.output(ledPin, GPIO.LOW) # pull down the pin
 
